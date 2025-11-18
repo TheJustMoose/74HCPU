4f595a196194c4f0726a535240b96197c515bc8a
TheJustMoose <justmoose@ya.ru>
2025-11-18 23:04:58 +0300

    test ByteOffsetToInt function

--- soft/uemul/unit_tests.cc
+++ soft/uemul/unit_tests.cc
@@ -13,3 +13,11 @@ TEST_CASE("test OffsetToInt") {
   CHECK(OffsetToInt(8) == -8);
   CHECK(OffsetToInt(15) == -1);
 }
+
+TEST_CASE("test ByteOffsetToInt") {
+  // 8 bit two's complement value
+  CHECK(ByteOffsetToInt(0) == 0);
+  CHECK(ByteOffsetToInt(1) == 1);
+  CHECK(ByteOffsetToInt(127) == 127);
+  CHECK(ByteOffsetToInt(128) == -128);
+}
