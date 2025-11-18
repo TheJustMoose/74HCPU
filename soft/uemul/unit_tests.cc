3e2e4de697866b1082c2de460b43b721c2ce99a4
TheJustMoose <justmoose@ya.ru>
2025-11-18 23:02:07 +0300

    test OffsetToInt function

--- soft/uemul/unit_tests.cc
+++ soft/uemul/unit_tests.cc
@@ -1,8 +1,15 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
 
+#include "offset2int.h"
+
 using namespace std;
 
-TEST_CASE("test") {
-  CHECK(true);
+TEST_CASE("test OffsetToInt") {
+  // 4 bit two's complement value
+  CHECK(OffsetToInt(0) == 0);
+  CHECK(OffsetToInt(1) == 1);
+  CHECK(OffsetToInt(7) == 7);
+  CHECK(OffsetToInt(8) == -8);
+  CHECK(OffsetToInt(15) == -1);
 }
