97bd42769fbe24fbecc199c80e1cd0c1292b65e9
TheJustMoose <justmoose@ya.ru>
2026-01-14 21:55:53 +0300

    Copy LCD drawer into build dir

--- soft/uemul/build.bat
+++ soft/uemul/build.bat
@@ -2,3 +2,5 @@ md bin
 cd bin
 cmake ..
 cmake --build . --config Release
+cd ..
+copy lcd\lcd.py ..\Release\lcd.py
--- soft/uemul/build.sh
+++ soft/uemul/build.sh
@@ -2,3 +2,4 @@ mkdir bin
 cd bin
 cmake ..
 cmake --build . --config Release
+#cp lcd/lcd.py ../Release/lcd.py
