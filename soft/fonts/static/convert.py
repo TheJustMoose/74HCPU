e1f1adaa40dc34080065175c20e7da9b1e95d79e
TheJustMoose <justmoose@ya.ru>
2026-01-20 23:59:12 +0300

    Try to convert ttf to bitmap

---
+++ soft/fonts/static/convert.py
@@ -0,0 +1,40 @@
+# pip install pillow
+
+from PIL import Image, ImageDraw, ImageFont
+
+def conv_fnt(image):
+    print(f"'{char}' image.size: {image.size}")
+    pixels = image.load()
+    w, h = image.size
+    for y in range(h):
+        for x in range(w):
+            r, g, b, a = pixels[x, y]
+            if r != g or g != b:
+                print("Not equal!");
+            print("%2X |" % a, end = " ")
+            if r != 0:
+                print("Alarm!!!")
+
+        print("")
+
+font_path = "RobotoMono-Regular.ttf"
+characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+font_size = 24
+
+font = ImageFont.truetype(font_path, font_size)
+
+for char in characters:
+    bbox = font.getbbox(char)
+    print(bbox)
+    image_width = bbox[2] - bbox[0] + 20  # paddings
+    image_height = bbox[3] - bbox[1] + 30
+    
+    # Create image with transparent font
+    image = Image.new("RGBA", (image_width, image_height), (0, 0, 0, 0))
+    draw = ImageDraw.Draw(image)
+    
+    # Draw char
+    draw.text((10, 10), char, font = font, fill = "black")
+    image = image.crop(image.getbbox())
+    conv_fnt(image)
+    image.save(f"chr-{char}-{ord(char)}.png")
