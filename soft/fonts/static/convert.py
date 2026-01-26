0e51b803dca2aa8d2773d6607c07c0f02105be74
TheJustMoose <justmoose@ya.ru>
2026-01-26 22:04:13 +0300

    Fix converter output

--- soft/fonts/static/convert.py
+++ soft/fonts/static/convert.py
@@ -6,7 +6,7 @@ def conv_fnt(image, char):
     print(f"; '{char}' image.size: {image.size}")
     pixels = image.load()
     w, h = image.size
-    print(f"glyph_{char} db {w}, {h}, \\ ; width, height, data...")
+    print(f".db glyph_{char} {w}, {h}, \\ ; width, height, data...")
     for y in range(h):
         for x in range(w):
             r, g, b, a = pixels[x, y]
