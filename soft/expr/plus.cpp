16ed8a95dfb811c185e8fa9447b644a3a3aa7baf
TheJustMoose <justmoose@ya.ru>
2026-03-05 22:35:28 +0300

    Try to rever parser and move Lexer to separate unit

--- soft/expr/plus.cpp
+++ soft/expr/plus.cpp
@@ -34,25 +34,8 @@ class FuncGuard {
   }
 };
 
-
-class Lexer {
- public:
-  Lexer(string s): input_string_(s) {}
-
-  char currentToken() {
-    return (idx_ < input_string_.size()) ?
-        input_string_[idx_] : 0;
-  }
-
-  void consume() {
-    if (idx_ < input_string_.size())
-      idx_++;
-  }
-
- private:
-  string input_string_ {};
-  size_t idx_ {0};
-};
+string input_string;
+size_t idx {0};
 
 int value {0};
 string name {""};
