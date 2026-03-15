5904505527b5a874a5803af35acbcd8031b224cb
TheJustMoose <justmoose@ya.ru>
2026-03-16 00:02:02 +0300

    Comment debug strings

--- soft/expr/print_tree.cc
+++ soft/expr/print_tree.cc
@@ -26,26 +26,26 @@ void Tree2List(Node* n, int mid, int lvl = 0) {
   gLinearTree.push_back(n);
   gNodeLevel[n] = lvl;
   gNodeOffset[n] = mid;
-  cout << "mid: " << mid << ", lvl: " << lvl << endl;
+  //cout << "mid: " << mid << ", lvl: " << lvl << endl;
 
   if (BinOp* bop = dynamic_cast<BinOp*>(n)) {
-    cout << "binop..." << endl;
+    //cout << "binop..." << endl;
     lvl++;
     int offset = kWidth / (lvl*2 + 5);
     Tree2List(bop->left, mid - offset, lvl);
     Tree2List(bop->right, mid + offset, lvl);
   } else if (AssignOp* aop = dynamic_cast<AssignOp*>(n)) {
-    cout << "assign..." << endl;
+    //cout << "assign..." << endl;
     lvl++;
     int offset = kWidth / (lvl*2 + 5);
     Tree2List(aop->left, mid - offset, lvl);
     Tree2List(aop->right, mid + offset, lvl);
   } else if (UnOp* uop = dynamic_cast<UnOp*>(n)) {
-    cout << "unary minus..." << endl;
+    //cout << "unary minus..." << endl;
     lvl++;
     Tree2List(uop->child, mid, lvl);
   } else {
-    cout << "Node: " << GetNodeTypeName(n->type()) << endl;
+    //cout << "Node: " << GetNodeTypeName(n->type()) << endl;
   }
 }
 
@@ -71,7 +71,7 @@ void PrintTree(Node* n) {
   gNodeLevel.clear();
 
   Tree2List(n, kWidth / 2);  // root node should be in a center of screen
-  cout << gLinearTree.size() << " items was stored" << endl;
+  //cout << gLinearTree.size() << " items was stored" << endl;
 
   string line(kWidth, ' ');
   int last_lvl {0};
@@ -82,7 +82,7 @@ void PrintTree(Node* n) {
     int lvl = gNodeLevel[n];
     if (last_lvl != lvl) {
       cout << line << endl;
-      cout << "The next level..." << endl;
+      //cout << "The next level..." << endl;
       line = string(kWidth, ' ');
       last_lvl = lvl;
       nodes_on_lvl = 1;  // we just found node on next level and node exist, so 'cnt' = 1
@@ -95,7 +95,7 @@ void PrintTree(Node* n) {
         GetNodeTypeName(n->type()) + " (" + to_string(gNodeOffset[n]) + ")",
         gNodeOffset[n]);
 
-    cout << "cnt: " << cnt << ", " << GetNodeTypeName(n->type()) << endl;
+    //cout << "cnt: " << cnt << ", " << GetNodeTypeName(n->type()) << endl;
     cnt++;
   }
 
