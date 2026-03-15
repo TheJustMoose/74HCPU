095ce607413d37c6816c42d59123751afd991629
TheJustMoose <justmoose@ya.ru>
2026-03-15 17:47:38 +0300

    Workin on tree print

--- soft/expr/print_tree.cc
+++ soft/expr/print_tree.cc
@@ -1,9 +1,12 @@
 #include "print_tree.h"
+#include "names.h"
 #include "node.h"
+#include "node_type.h"
 #include "nodes.h"
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -14,18 +17,29 @@ string dup(char c, int w) {
   return res;
 }
 
-void Tree2List(Node* n) {
+vector<Node*> gLinearTree;
+
+void Tree2List(Node* n, int lvl = 0) {
+  NodeType nt = n->type();
+  cout << "node type: " << GetNodeTypeName(nt) << ", lvl: " << lvl << endl;
+  gLinearTree.push_back(n);
+
+  if (BinOp* bop = dynamic_cast<BinOp*>(n)) {
+    Tree2List(bop->left, lvl + 1);
+    Tree2List(bop->right, lvl + 1);
+  }
+
+  if (AssignOp* aop = dynamic_cast<AssignOp*>(n)) {
+    Tree2List(aop->left, lvl + 1);
+    Tree2List(aop->right, lvl + 1);
+  }
 }
 
 void PrintTree(Node* n) {
   if (!n)
     return;
 
-  cout << dup(' ', width / 2) << n->op() << endl;
-
-  if (BinOp* bop = dynamic_cast<BinOp*>(n)) {
-  }
-
-  if (AssignOp* aop = dynamic_cast<AssignOp*>(n)) {
-  }
+  //cout << dup(' ', width / 2) << n->op() << endl;
+  Tree2List(n);
+  cout << endl;
 }
