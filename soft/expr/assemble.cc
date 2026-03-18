4399bdfbda4e2ec8ea410f55e75e1a5ac3a9d88c
TheJustMoose <justmoose@ya.ru>
2026-03-18 22:53:15 +0300

    Bugfixing

--- soft/expr/assemble.cc
+++ soft/expr/assemble.cc
@@ -31,13 +31,20 @@ size_t reg_cnt = 8;
 vector<string> bank0(reg_cnt);
 vector<string> bank1(reg_cnt);
 
-string FindFreeRegFor(string var_name) {
+string FindRegFor(string var_name) {
+  // check existent pair var:reg
+  for (size_t i = 0; i < 8; i++)
+    if (bank0[i] == var_name)
+      return "R" + to_string(i);
+
+  // try to insert new pair var:reg
   for (size_t i = 0; i < 8; i++)
     if (!bank0[i].size()) {
       bank0[i] = var_name;
       return "R" + to_string(i);
     }
 
+  // oops
   return "";
 }
 
@@ -60,12 +67,12 @@ vector<string> Assemble(vector<Operation> code) {
   for (Operation op : code) {
     cout << op.raw() << " " << op.str() << endl;
     if (!op.op_name.size()) {
-      string res_reg = FindFreeRegFor(op.res_arg);
+      string res_reg = FindRegFor(op.res_arg);
       string line1 = op.res_arg + " = " + op.left_arg;
       res.push_back(line1);
 
       string line11 = "mov " + res_reg + ", " +
-                      (op.left_arg_is_num ? op.left_arg : FindFreeRegFor(op.left_arg)) +
+                      (op.left_arg_is_num ? op.left_arg : FindRegFor(op.left_arg)) +
                       "   " + DumpRegs();
       res.push_back(line11);
     } else if (!op.left_arg.size()) {  // t1 = -t1
@@ -76,12 +83,28 @@ vector<string> Assemble(vector<Operation> code) {
 
       res.push_back("asm will be here soon (op.left_arg is empty)");
     } else {
-      string line1 = op.res_arg + " = " + op.left_arg;
+      string res_reg = FindRegFor(op.res_arg);
+
+      string line1 = res_reg + " = " + op.left_arg;
       res.push_back(line1);
-      string line2 = op.res_arg + " " + op.op_name + "= " + op.right_arg;
+      string line2 = res_reg + " " + op.op_name + "= " + op.right_arg;
       res.push_back(line2);
 
-      res.push_back("asm will be here soon (else)");
+      string line11 = "mov " + res_reg + ", " + op.left_arg;
+      res.push_back(line11);
+
+      string cmd {"unk"};
+      if (op.op_name == "+")
+        cmd = "add";
+      else if (op.op_name == "-")
+        cmd = "sub";
+      else if (op.op_name == "*")
+        cmd = "mul";
+      else if (op.op_name == "/")
+        cmd = "/ - not implemented!";
+
+      string line21 = cmd + " " + res_reg + ", " + op.right_arg;
+      res.push_back(line21);
     }
   }
 
