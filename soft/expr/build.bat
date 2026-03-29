862c2bac4e70cb5e2f6d8498071a118c8a3d7ef2
TheJustMoose <justmoose@ya.ru>
2026-03-29 23:42:40 +0300

    Fix build script

--- soft/expr/build.bat
+++ soft/expr/build.bat
@@ -1,6 +1,9 @@
 git submodule update
 md bin
 cd bin
+del "..\..\Release\simple_expr.exe"
+del "..\..\Release\simple_expr_unit_tests.exe"
+
 cmake ..
 cmake --build . --config Release
 REM cmake --build . --config Debug
