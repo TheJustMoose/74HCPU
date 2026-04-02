105b95963ac6fc01c4c0b8a76fef0279c22ebd2d
TheJustMoose <justmoose@ya.ru>
2026-04-02 23:06:14 +0300

    First mock test is Ok!

--- soft/expr/regs_bank_unit_tests.cc
+++ soft/expr/regs_bank_unit_tests.cc
@@ -5,6 +5,7 @@
 using trompeloeil::_;
 
 #include "regs_bank.h"
+#include "spillable.h"
 
 using namespace std;
 
@@ -45,7 +46,7 @@ TEST_CASE("check RegsBank0::FindRegFor with many variables") {
       CHECK_EQ(reg, "R" + si);
   }
 }
-/*
+
 class MockSpillable : public ISpillable {
  public:
   // this macros will create mock method:
@@ -53,7 +54,14 @@ class MockSpillable : public ISpillable {
 };
 
 TEST_CASE("check RegsBank0::Spill") {
+  MockSpillable mockSpill;
+  RegsBank0 rb {&mockSpill};
 
-  CHECK(false);
+  // mockSpill::Spill should be called with any args (trompeloeil::_) two times
+  REQUIRE_CALL(mockSpill, Spill(trompeloeil::_, trompeloeil::_)).TIMES(2);
+
+  vector<string> res_code;
+  for (int i = 0; i < 10; i++) {
+    string reg = rb.FindRegFor("var" + to_string(i), res_code);
+  }
 }
-*/
