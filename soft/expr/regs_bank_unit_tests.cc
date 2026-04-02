b512cec211260e5357c706d9a560aa920910b5b4
TheJustMoose <justmoose@ya.ru>
2026-04-02 18:41:36 +0300

    Testing RegsBank...

--- soft/expr/regs_bank_unit_tests.cc
+++ soft/expr/regs_bank_unit_tests.cc
@@ -5,6 +5,23 @@
 using namespace std;
 
 TEST_CASE("check C++") {
-  //RegsBank0
-  CHECK(false);
+  RegsBank0 rb;
+  CHECK(rb[0].empty());
+
+  vector<string> res_code;
+
+  string reg = rb.FindRegFor("var", res_code);
+  CHECK_EQ(reg, "R0");
+
+  reg = rb.FindRegFor("abc", res_code);
+  CHECK_EQ(reg, "R1");
+
+  reg = rb.FindRegFor("def", res_code);
+  CHECK_EQ(reg, "R2");
+
+  reg = rb.FindRegFor("i", res_code);
+  CHECK_EQ(reg, "R3");
+
+  reg = rb.FindRegFor("i", res_code);
+  CHECK_EQ(reg, "R3");  // have to get same register for same variable
 }
